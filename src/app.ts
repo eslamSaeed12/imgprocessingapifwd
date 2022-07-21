978b53d809e977a1ae9ce99ea32047e3e43202c6
eslamSaeed12 <islam.s.mhmd@gmail.com>
2022-07-21 12:54:16 +0200

    fix linting error

--- src/app.ts
+++ src/app.ts
@@ -1,61 +1,58 @@
-import type { Request, Response, NextFunction } from 'express'
-import express from 'express'
-import { FileHelper } from './modules/@file-helper'
-import { ImgTransformer } from './modules/@img-transformer'
-import { join } from 'path'
+import type { Request, Response, NextFunction } from 'express';
+import express from 'express';
+import { FileHelper } from './modules/@file-helper';
+import { ImgTransformer } from './modules/@img-transformer';
+import { join } from 'path';
 
-const imgHelper = new FileHelper(join(process.cwd(), 'images'))
+const imgHelper = new FileHelper(join(process.cwd(), 'images'));
 
-const imgTransformer = new ImgTransformer()
+const imgTransformer = new ImgTransformer();
 
-const app = express()
+const app = express();
 
-// just to check application is running
-app.get('/', (req, res) => {
-  res.send('Img Processing Api FWD Udacity Project 1')
-})
+
+// end point to check api helath status
+app.get('/api/health', (req: Request, res: Response) => {
+  res.send('Img Processing Api FWD Udacity Project 1');
+});
 
 // getting an image with required dimensions
-app.get(
-  '/api/images',
-  async function (req: Request, res: Response, next: NextFunction) {
-    try {
-      const { filename, width, height } = req.query
+app.get('/api/images', async function (req: Request, res: Response, next: NextFunction) {
+  try {
+    const { filename, width, height } = req.query;
 
-      if (!filename) {
-        return res.status(400).send('Filename is required')
-      }
-
-      if (!width || !height) {
-        return res.status(400).send('Width and Height is A required !')
-      }
-
-      if (!imgHelper.isImgExist(<string>filename)) {
-        return res.status(404).send('Image is Not Found !')
-      }
-
-      const img = imgHelper.getImg(<string>filename)
-
-      const output = await imgTransformer.transform({
-        img,
-        width: parseInt(<string>width),
-        height: parseInt(<string>height),
-      })
-
-
-      // cahing the image for 24 hours
-      res.setHeader('Cache-Control', 'public, max-age=86400000');
-      res.setHeader('Content-Type', 'image/jpeg');
-      res.send(output)
-    } catch (err) {
-      next(err)
+    if (!filename) {
+      return res.status(400).send('Filename is required');
     }
+
+    if (!width || !height) {
+      return res.status(400).send('Width and Height is A required !');
+    }
+
+    if (!imgHelper.isImgExist(<string>filename)) {
+      return res.status(404).send('Image is Not Found !');
+    }
+
+    const img = imgHelper.getImg(<string>filename);
+
+    const output = await imgTransformer.transform({
+      img,
+      width: parseInt(<string>width),
+      height: parseInt(<string>height),
+    });
+
+    // cahing the image for 24 hours
+    res.setHeader('Cache-Control', 'public, max-age=86400000');
+    res.setHeader('Content-Type', 'image/jpeg');
+    res.send(output);
+  } catch (err) {
+    next(err);
   }
-)
+});
 
 // error handler middleware for showing error message
 app.use((err: Error, req: Request, res: Response) => {
-  res.status(500).send(err.message)
-})
+  res.status(500).send(err.message);
+});
 
-export { app }
+export { app };
